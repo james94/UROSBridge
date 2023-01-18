9906637354dfc8dce290d3833c51141a4db4a660
Michael Neumann <michael_neumann@gmx.net>
2023-01-18 10:58:38 +0100

    update log msg

--- Source/UROSBridge/Private/ROSSubsystem.cpp
+++ Source/UROSBridge/Private/ROSSubsystem.cpp
@@ -4,7 +4,7 @@
 
 void IRosInterface::Connect(const FString& InIp, const int32& InPort)
 {
-  UE_LOG(LogROS, Log, TEXT("Connecting to %s:%d via GameInstance"), *InIp, InPort);
+  UE_LOG(LogROS, Log, TEXT("Connecting to %s:%d via Subsystem"), *InIp, InPort);
   ROSHandler = MakeShareable<FROSBridgeHandler>(new FROSBridgeHandler(InIp, InPort));
   ROSHandler->Connect();
 }
